e70aceebfc3509fad645eb3a6653fa339b6f026b
Sheblyk <kokosigel@gmail.com>
2021-07-27 16:57:46 +0300

    created main logic: added hql request to get next lesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().configure();
-        try(SessionFactory sessionFactory = configuration.buildSessionFactory()){
-            System.out.println("Working");
-        }
-
+        GetNextLesson nextLesson = new GetNextLesson();
+        nextLesson.get();
     }
 }
